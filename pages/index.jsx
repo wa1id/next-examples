10ca0946e806e12e037dc1f67c44828e03313d4b
Wa1iD <wyachou95@gmail.com>
2023-06-01 13:57:20 +0200

    feat: add Head

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,12 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Next.js Demo</title>
+      </Head>
       <header>
         <nav className="p-8">
           <ul className="list-disc list-inside">
